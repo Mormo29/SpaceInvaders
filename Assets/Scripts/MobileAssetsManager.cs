96ee45e8a384d5efb2986354015fedf12403d879
nestorzac <a.perez@uteca.edu.mx>
2026-07-20 20:35:05 -0600

    Gerryestuvoaqui

--- Assets/Scripts/MobileAssetsManager.cs
+++ Assets/Scripts/MobileAssetsManager.cs
@@ -7,19 +7,27 @@ public class MobileAssetsManager : MonoBehaviour
    [SerializeField]
    private GameObject[] desktopAssets; 
    private void Awake()
-    {
-        #if UNITY_IOS|| UNITY_ANDROID
-         foreach (var asset in mobileAssets)
-         {
-            asset.SetActive(true);
-         }
-        #else
-         foreach (var asset in desktopAssets)
-         {
-            asset.SetActive(true);
-         }
-        #endif
-    }
+   {
+        if (Application.isEditor)
+      {
+         SetActiveAssets(desktopAssets, true);
+      }
+      else if (Application.isMobilePlatform)
+      {
+         SetActiveAssets(mobileAssets, true);
+      }
+      else
+      {
+         SetActiveAssets(desktopAssets, true);
+      }
+   }
+   private void SetActiveAssets(GameObject[] assets, bool isActive)
+   {
+      foreach (var asset in assets)
+      {
+         asset.SetActive(isActive);
+      }
+   }
    
 
 }
